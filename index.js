1856fa68aae5fd735cf7f81ff7f92d6242f9cd94
Ty <htl981790867@gmail.com>
2023-05-08 16:05:02 -0700

    6 commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(PORT, () => {
     console.log(`listing on port ${PORT}`);
 });
 
-cron.schedule("0 15 * * *", function() {
+cron.schedule("10 16 * * *", function() {
     console.log("running cron job for scrapeCamden.....");
     let no_res = "n";
     scrapeCamdenOneBed(no_res);
